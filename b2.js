f5ce92c395cf8788ef57b02b78715433bc1828ec
latp1102 <latp1102@gmail.com>
2025-02-26 11:14:33 +0700

    Session16-2

---
+++ b2.js
@@ -0,0 +1,8 @@
+let number = 10;
+let price = 20.5;
+let status = true;
+let city = "Hà Nội"
+document.write("<p>Biến number: " + number + "</p>");
+document.write("<p>Biến price: " + price + "</p>");
+document.write("<p>Biến status:" + status +" </p>");
+document.write("<p>Biến city: " + city + " </p>");
