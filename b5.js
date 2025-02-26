33c2b314e5b94783c37c90946ea8e1bf8595dacf
latp1102 <latp1102@gmail.com>
2025-02-26 11:16:55 +0700

    Session16-5

---
+++ b5.js
@@ -0,0 +1,9 @@
+let firstNumber = prompt("Nhập số thứ nhất:");
+let secondNumber = prompt("Nhập số thứ hai:");
+
+
+firstNumber = parseInt(firstNumber);
+secondNumber = parseInt(secondNumber);
+
+alert(firstNumber + secondNumber);
+
